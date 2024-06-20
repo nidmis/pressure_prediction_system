cee5487909acbe538d36e9b8b4e0e6a9e4a3742e
NIDHI_MISHRA <119472718+nidmis@users.noreply.github.com>
2024-06-20 20:19:43 +0530

    Add files via upload

---
+++ cloud.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const cloud = document.querySelector('.cloud');
+    cloud.style.animationDuration = `${Math.random() * 10 + 10}s`;
+});
